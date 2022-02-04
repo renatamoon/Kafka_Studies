a7d943a96e67aead101a9bd93fa2222778c3d78c
renatamonteiro <rcm@lionx.com.br>
2022-02-04 10:57:42 -0300

    adding consummer

--- Kafka - FullCycle/producer.py
+++ Kafka - FullCycle/producer.py
@@ -5,15 +5,22 @@ from data import get_registered_user
 import time
 
 # the finallity of this producer is to send the data to the topic that will be partioned
-
 # instancing the KafkaProducer to generate the data
 def json_serializer(data):
     return json.dumps(data).encode("utf-8")
 
 
+# everytime the producer code is called, they will use the partition 0 to publish the messages
+# with this, there will be no message be sending to the partition 1, cause we are already sending it to
+# the partition 0
+def get_partition(key, all, available):
+    return 0
+
+
 # local host is the broker address
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                            value_serializer=json_serializer)
+                            value_serializer=json_serializer,
+                            partitioner=get_partition)
 
 
 # everytime this registered loop is working, will be getting a new registered user
